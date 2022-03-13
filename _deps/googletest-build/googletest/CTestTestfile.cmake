407203bb0a4d8eeeeae2944c19715138c9df4f44
v0juse <95091718+v0juse@users.noreply.github.com>
2022-03-13 13:32:39 -0300

    Delete _deps directory

--- _deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/v0id/Faculdade/Testes/Elevador/_deps/googletest-src/googletest
-# Build directory: /home/v0id/Faculdade/Testes/Elevador/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
